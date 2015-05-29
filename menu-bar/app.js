e855f95fed4b2a0888dff3a9a01c7e7bf1aff624
Hai Do Minh <minhhai3b@gmail.com>
2015-05-30 08:21:42 +0900

    add event for menu-bar

--- menu-bar/app.js
+++ menu-bar/app.js
@@ -5,3 +5,27 @@ var mb = menubar({dir: __dirname})
 mb.on('ready', function ready() {
   console.log('app is ready')
 })
+
+mb.on('create-window', function () {
+  console.log('creating window')
+})
+
+mb.on('after-create-window', function() {
+  console.log('created window')
+})
+
+mb.on('show', function() {
+  console.log('window.show is called')
+})
+
+mb.on('after-show', function() {
+  console.log('window is shown')
+})
+
+mb.on('hide', function() {
+  console.log('window.hide is called')
+})
+
+mb.on('after-hide', function() {
+  console.log('window is hided')
+})
